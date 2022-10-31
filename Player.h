e4854246dc93587404543d5c4a338438495ad27c
Kazuma <210678@jc-21.jp>
2022-10-31 16:37:23 +0900

    Playerクラス

--- Player.h
+++ Player.h
@@ -1,5 +1,27 @@
 #pragma once
-class Player
+#include "Engine/GameObject.h"
+
+//◆◆◆を管理するクラス
+class Player : public GameObject
 {
+
+public:
+    //コンストラクタ
+    Player(GameObject* parent);
+
+    //デストラクタ
+    ~Player();
+
+    //初期化
+    void Initialize() override;
+
+    //更新
+    void Update() override;
+
+    //描画
+    void Draw() override;
+
+    //開放
+    void Release() override;
 };
 
