8a428bde7f21eeba7e750294d91770302e09d75e
Kazuma <210678@jc-21.jp>
2022-11-07 16:22:31 +0900

    player表示

--- PlayScene.cpp
+++ PlayScene.cpp
@@ -1,5 +1,7 @@
 #include "PlayScene.h"
 #include "Engine/Image.h"
+#include "Player.h"
+#include "Engine/Input.h"
 
 //コンストラクタ
 PlayScene::PlayScene(GameObject* parent)
@@ -10,20 +12,39 @@ PlayScene::PlayScene(GameObject* parent)
 //初期化
 void PlayScene::Initialize()
 {
+
 	hPict_ = Image::Load("Assets/Shinwa.jpg");
 	assert(hPict_ >= 0);
+
+	Instantiate<Player>(this);
 }
 
 //更新
 void PlayScene::Update()
 {
+	if (Input::IsKey(DIK_W))
+	{
+		transform_.position_.y += 0.01;
+	}
+	if (Input::IsKey(DIK_S))
+	{
+		transform_.position_.y -= 0.01;
+	}
+	if (Input::IsKey(DIK_D))
+	{
+		transform_.position_.x += 0.01;
+	}
+	if (Input::IsKey(DIK_A))
+	{
+		transform_.position_.x -= 0.01;
+	}
 }
 
 //描画
 void PlayScene::Draw()
 {
-	Image::SetTransform(hPict_, transform_);
-	Image::Draw(hPict_);
+	/*Image::SetTransform(hPict_, transform_);
+	Image::Draw(hPict_);*/
 }
 
 //開放
